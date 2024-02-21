554bce0fc7ad7f6421b93f2173a51d475291703d
成舟 <1738248438@qq.com>
2024-02-21 11:13:51 +0800

    update: prettier配置文件更新

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,10 +1,21 @@
 export default {
-  trailingComma: 'none',
+  // 末尾需要有逗号
+  trailingComma: 'all',
+  // 使用 2 个空格缩进
   tabWidth: 2,
+  // 行尾不需要有分号
   semi: false,
+  // 一行最多 120 字符
   printWidth: 120,
+  printWidth: 120,
+  // 使用单引号
   singleQuote: true,
+  // 大括号内的首尾需要空格
   bracketSpacing: true,
+  // 箭头函数，只有一个参数的时候，也需要括号
+  arrowParens: 'always',
+  // 根据显示样式决定 html 要不要折行
   htmlWhitespaceSensitivity: 'ignore',
-  endOfLine: 'lf'
+  // 换行符使用 lf
+  endOfLine: 'lf',
 }
