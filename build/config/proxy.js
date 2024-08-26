aa32b738abbd76728381969c602d2468938472e2
成舟 <1738248438@qq.com>
2024-08-26 10:00:59 +0800

    update: 代理详解

--- build/config/proxy.js
+++ build/config/proxy.js
@@ -1,8 +1,9 @@
 const proxyServer = {
+  // api地址匹配的字符串，可以使用正则，此处表示以/api为开头的接口地址都会被代理
   '/api': {
-    target: 'http://localhost:3000',
-    changeOrigin: true,
-    rewrite: (path) => path.replace(/^\/api/, '')
+    target: 'http://localhost:3000', // 指向，表示上述需要匹配的地址都指向这个域名，注意要用/结尾
+    changeOrigin: true, // 是否修改请求头的origin，让服务器认为这个请求来自本域名
+    rewrite: (path) => path.replace(/^\/api/, '') // 如果匹配字符不需要了，可以使用重写去掉
   }
   // 字符串简写写法
   // '/foo': 'http://localhost:4567',
